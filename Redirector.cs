aa553080a0db1a0d74fd1fa8e48e73e461896349
Egor Aralov <aei.omsk@gmail.com>
2017-07-09 21:16:58 +0200

    Add code

---
+++ Redirector.cs
@@ -0,0 +1,22 @@
+﻿using RedirectionFramework.Extensions;
+
+namespace RedirectionFramework
+{
+    public class Redirector<T>
+    {
+        public static void Deploy()
+        {
+            typeof(T).Redirect();
+        }
+
+        public static void Revert()
+        {
+            typeof(T).Revert();
+        }
+
+        public static bool IsDeployed()
+        {
+            return typeof(T).IsRedirected();
+        }
+    }
+}
