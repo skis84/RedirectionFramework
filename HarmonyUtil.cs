7905972117f9470693fb707e78b5f49eb5e75370
kian.zarrin <kian.zarrin@gmail.com>
2020-04-25 20:47:00 +0300

    pushed TMPE to Harmony 2

--- HarmonyUtil.cs
+++ HarmonyUtil.cs
@@ -1,22 +1,19 @@
-﻿using Harmony;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using HarmonyLib;
 using System.Reflection;
-using System.Text;
 
 namespace TrafficManager.RedirectionFramework {
-	public static class HarmonyUtil {
-		/// <summary>
-		/// Finds the method that is represented by the given harmony method.
-		/// </summary>
-		/// <param name="method">queried harmony method</param>
-		/// <returns>method information</returns>
-		public static MethodBase GetOriginalMethod(HarmonyMethod method) {
-			if (method.declaringType == null) return null;
-			if (method.methodName == null)
-				return AccessTools.Constructor(method.declaringType, method.argumentTypes);
-			return AccessTools.Method(method.declaringType, method.methodName, method.argumentTypes);
-		}
-	}
+    public static class HarmonyUtil {
+        /// <summary>
+        /// Finds the method that is represented by the given harmony method.
+        /// </summary>
+        /// <param name="method">queried harmony method</param>
+        /// <returns>method information</returns>
+        public static MethodBase GetOriginalMethod(object method) { // use object for Cities Harmony compatibality.
+            HarmonyMethod actualMethod = method as HarmonyMethod;
+            if (actualMethod.declaringType == null) return null;
+            if (actualMethod.methodName == null)
+                return AccessTools.Constructor(actualMethod.declaringType, actualMethod.argumentTypes);
+            return AccessTools.Method(actualMethod.declaringType, actualMethod.methodName, actualMethod.argumentTypes);
+        }
+    }
 }
